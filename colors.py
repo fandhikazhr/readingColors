585b83263deb71c36cb638c7d9b58a85a137edf9
Fandhika Azhar <99522867+fandhikazhr@users.noreply.github.com>
2022-03-08 04:15:47 +0700

    Update colors.py

--- colors.py
+++ colors.py
@@ -66,6 +66,35 @@ while True:
         k = cv2.waitKey(5)
         if k == 27:
             break
-        # 0, 50, 50
-        # 10, 255, 255
-        
+    # 0, 50, 50
+    # 10, 255, 255
+    lower_red = np.array([0, 90, 70], np.uint8) # setting the red lower limit
+    upper_red = np.array([67, 255, 255], np.uint8) # setting the re upper limit
+    red = cv2.inRange(hsv, lower_red, upper_red)
+
+    cnts3 = cv2.findContours(red, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    cnts3 = imutils.grab_contours(cnts3)
+
+    for r in cnts3:
+        area = cv2.contourArea(r)
+        if(area > 5000):
+            cv2.drawContours(frame, [r], -1, (0, 255, 0), 3)
+
+            M = cv2.moments(r)
+
+            cx = int(M["m10"] / M["m00"])
+            cy = int(M["m01"] / M["m00"])
+
+            cv2.circle(frame, (cx, cy), 7, (255, 255, 255), -1)
+            cv2.putText(frame, "Centre", (cx - 20, cy - 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
+            print("area ", area)
+            print("the coordinate is..", cx, cy)
+
+        cv2.imshow("frame", frame)
+
+        k = cv2.waitKey(5)
+        if k == 27:
+            break
+
+cap.release()
+cv2.destroyAllWindows()
