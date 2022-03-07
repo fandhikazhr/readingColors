5cee49c8ae430f7a6086d8aa8a2447a96c61784c
Fandhika Azhar <99522867+fandhikazhr@users.noreply.github.com>
2022-03-08 04:14:23 +0700

    Update colors.py

--- colors.py
+++ colors.py
@@ -32,10 +32,40 @@ while True:
             print("area ", area)
             print("the coordinate is..", cx, cy)
 
-    cv2.imshow("frame", frame)
+        cv2.imshow("frame", frame)
 
-    k = cv2.waitKey(5)
-    if k == 27:
+        k = cv2.waitKey(5)
+        if k == 27:
         break
     # 30, 82, 72
     # 102, 255, 255
+    lower_green = np.array([30, 82, 72], np.uint8)  # setting the green lower limit
+    upper_green = np.array([102, 255, 255], np.uint8)  # setting the green upper limit
+    green = cv2.inRange(hsv, lower_green, upper_green)
+
+    cnts2 = cv2.findContours(green, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    cnts2 = imutils.grab_contours(cnts2)
+
+    for g in cnts2:
+        area = cv2.contourArea(g)
+        if (area > 5000):
+            cv2.drawContours(frame, [g], -1, (0, 255, 0), 3)
+
+            M = cv2.moments(g)
+
+            cx = int(M["m10"] / M["m00"])
+            cy = int(M["m01"] / M["m00"])
+
+            cv2.circle(frame, (cx, cy), 7, (255, 255, 255), -1)
+            cv2.putText(frame, "Centre", (cx - 20, cy - 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
+            print("area ", area)
+            print("the coordinate is..", cx, cy)
+
+        cv2.imshow("frame", frame)
+
+        k = cv2.waitKey(5)
+        if k == 27:
+            break
+        # 0, 50, 50
+        # 10, 255, 255
+        
