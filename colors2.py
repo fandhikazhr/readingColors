48aedb2d511c0daeb239687c42849717137bde71
Fandhika Azhar <99522867+fandhikazhr@users.noreply.github.com>
2022-03-11 02:35:44 +0700

    Update colors2.py

--- colors2.py
+++ colors2.py
@@ -12,3 +12,19 @@ upper_red = np.array([10, 255, 255], np.uint8) # setting the re upper limit
 cap = cv2.VideoCapture(0)
 cap.set(3, 630)
 cap.set(4, 360)
+
+while True:
+    success, frame = cap.read()
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    blue = cv2.inRange(hsv, lower_blue, upper_blue)
+    cnts1 = cv2.findContours(blue, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    countours1 = imutils.grab_contours(cnts1)
+
+    for blueCountour in countours1:
+        area1 = cv2.contourArea(blueCountour)
+        if (area1 > 5000):
+            cv2.drawContours(frame, [blueCountour], -1, (0, 255, 0), 3)
+            M = cv2.moments(blueCountour)
+            cx = int(M["m10"] / M["m00"])
+            cy = int(M["m01"] / M["m00"])
+            cv2.circle(frame, (cx, cy), 7, (255, 255, 255), -1)
